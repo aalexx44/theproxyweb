d070c1b1f3111619e41b8a169fa3fffb4782c63a
alex <aalexx.ic@gmail.com>
2023-05-17 00:30:49 +0200

    card, titlecard components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import BackgroundAnimated from './components/BackgroundAnimated'
+import Card from './components/Card'
+import TitleCard from './components/TitleCard'
 
 function App() {
   return (
     <>
       <BackgroundAnimated/>
+      <TitleCard title='Luces de Falso Contacto'/>
+      <Card/>
+      <TitleCard title='<3 Bienvenido a tu Destino <3'/>
     </>
   )
 }
