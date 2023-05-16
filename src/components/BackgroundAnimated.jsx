1c6c14608c53d44c4c88a819645ed099a2a81d5f
alex <aalexx.ic@gmail.com>
2023-05-16 21:18:30 +0200

    BackgroundAnimated error video solved

--- src/components/BackgroundAnimated.jsx
+++ src/components/BackgroundAnimated.jsx
@@ -1,11 +1,11 @@
-import videoBg from '../assets/animacion.mp4'
+import videoBg from '../assets/animacion.webm'
 import './BackgroundAnimated.css'
 
 function BackgroundAnimated() {
     return (
         <>
             <video muted autoPlay loop>
-                <source src={videoBg} type='video/mp4'/>
+                <source src={videoBg} type='video/webm'/>
             </video>
 
             <div className='layer'></div>
