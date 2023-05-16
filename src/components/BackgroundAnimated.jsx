e6e2e0f090e62133a56e9f915c0441ffd7118c8e
alex <aalexx.ic@gmail.com>
2023-05-16 21:51:49 +0200

    BackgroundAnimated update

--- src/components/BackgroundAnimated.jsx
+++ src/components/BackgroundAnimated.jsx
@@ -1,11 +1,11 @@
-import videoBg from '../assets/animacion.webm'
+import videoBg from '../assets/animacion.mp4'
 import './BackgroundAnimated.css'
 
 function BackgroundAnimated() {
     return (
         <>
             <video muted autoPlay loop>
-                <source src={videoBg} type='video/webm'/>
+                <source src={videoBg} type='video/mp4'/>
             </video>
 
             <div className='layer'></div>
