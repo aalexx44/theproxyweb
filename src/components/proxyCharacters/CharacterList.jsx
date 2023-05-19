e2f290b90905647508f8ad6141107df2d8e17b67
alex <aalexx.ic@gmail.com>
2023-05-19 13:41:17 +0200

    css charlist

--- src/components/proxyCharacters/CharacterList.jsx
+++ src/components/proxyCharacters/CharacterList.jsx
@@ -13,7 +13,7 @@ function CharacterList() {
 
 
     return (
-        <div className='bg-main mt-6 w-11/12 md:w-1/2 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 justify-items-center p-5 md:p-10'>
+        <div className='md:bg-main mt-6 w-11/12 md:w-1/2 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 justify-items-center p-5 md:p-10'>
             {data.map(item => (
             <CharacterCard key={item.name} name={item.name} url={vacio} />
             ))}
